fbb93071accc6b2d08efb7871d72de489f66383f
daniellyk <danielly.grando@escola.pr.gov.br>
2026-09-17 12:04:56 -0300

    ajustado algumas coisinhas na pagina de criar posts

--- script.js
+++ script.js
@@ -15,25 +15,5 @@ toggleButton.addEventListener('click', () => {
   }
 });
 
-document.addEventListener('DOMContentLoaded', () => {
-  const passwordInput = document.getElementById('reg-password');
-  const toggleButton = document.getElementById('toggle-password');
-  const eyeOpen = document.getElementById('eye-open');
-  const eyeClosed = document.getElementById('eye-closed');
 
-  if (!passwordInput || !toggleButton) return;
 
-  toggleButton.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    if (passwordInput.type === 'password') {
-      passwordInput.type = 'text';
-      eyeClosed.classList.add('hidden');
-      eyeOpen.classList.remove('hidden');
-    } else {
-      passwordInput.type = 'password';
-      eyeOpen.classList.add('hidden');
-      eyeClosed.classList.remove('hidden');
-    }
-  });
-});
